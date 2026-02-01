653a8336da05fef62bee4fa8f561c679c93b9620
diana030903 <144232137+diana030903@users.noreply.github.com>
2026-02-02 00:19:45 +0600

    repo: reorganize project into monorepo structure

--- around/test/widget_test.dart
+++ around/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:around/main.dart';
+import '../feature/frontend/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
